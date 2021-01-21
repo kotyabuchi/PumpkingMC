72f2867d4e3a74e6e5c69f62fad6846da4070ac2
kunugiya <kunugiya6280@gmail.com>
2021-01-21 15:00:44 +0900

    Add: Rarityに応じて耐久度がスケーリングするように

--- src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Enum/Rarity.kt
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Enum/Rarity.kt
@@ -1,9 +1,9 @@
 package com.github.kotyabuchi.pumpkingmc.Enum
 
-enum class Rarity(val text: String) {
-    COMMON("&fCOMMON"),
-    RARE("&9RARE"),
-    EPIC("&5EPIC"),
-    ANCIENT("&cANCIENT"),
-    LEGENDARY("&6LEGENDARY")
+enum class Rarity(val text: String, val durabilityMultiple: Double) {
+    COMMON("&fCOMMON", 1.0),
+    RARE("&9RARE", 1.25),
+    EPIC("&5EPIC", 2.0),
+    ANCIENT("&cANCIENT", 3.0),
+    LEGENDARY("&6LEGENDARY", 5.0)
 }
