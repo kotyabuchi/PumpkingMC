6a7cfe297d7aab71bd5582c0a083aae0058e76e1
kotyabuchi <kotyabuchi@gmail.com>
2021-01-25 19:42:47 +0900

    Change: クモをMobExpansionMasterの子クラスに変更

--- src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Entity/Monster/EnchantedSpider.kt
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Entity/Monster/EnchantedSpider.kt
@@ -3,50 +3,32 @@ package com.github.kotyabuchi.pumpkingmc.Entity.Monster
 import com.github.kotyabuchi.pumpkingmc.Utility.jump
 import com.github.kotyabuchi.pumpkingmc.instance
 import org.bukkit.Material
-import org.bukkit.entity.CaveSpider
+import org.bukkit.entity.EntityType
 import org.bukkit.entity.FallingBlock
 import org.bukkit.entity.Spider
 import org.bukkit.event.EventHandler
-import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityChangeBlockEvent
 import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.event.entity.EntityDropItemEvent
-import org.bukkit.event.entity.EntityTargetLivingEntityEvent
 import org.bukkit.scheduler.BukkitRunnable
 
-class EnchantedSpider: Listener {
-    
-    @EventHandler
-    fun onTarget(event: EntityTargetLivingEntityEvent) {
-        val spider = event.entity as? Spider ?: return
-        if (spider is CaveSpider) return
-        object: BukkitRunnable() {
-            override fun run() {
-                val target = spider.target
-                if (spider.isDead  || spider.target == null || target == null) {
-                    cancel()
-                } else {
-                    val spiderLoc = spider.location.toVector()
-                    val targetLoc = target.location.add(0.0, 0.5, 0.0).toVector()
-                    val webLocation = targetLoc.clone().subtract(spiderLoc).normalize().multiply(0.3)
-                    val web = spider.world.spawnFallingBlock(spider.location.add(webLocation), Material.COBWEB.createBlockData())
-                    web.velocity = targetLoc.clone().subtract(spiderLoc).multiply(0.2)
-                }
-            }
-        }.runTaskTimer(instance, 10, 20 * 3)
-    
-        object: BukkitRunnable() {
-            override fun run() {
-                val target = spider.target
-                if (spider.isDead  || spider.target == null || target == null) {
-                    cancel()
-                } else {
-                    spider.jump(target.location)
-                }
-            }
-        }.runTaskTimer(instance, 20, 20 * 2)
+class EnchantedSpider: MobExpansionMaster(EntityType.SPIDER, EntityType.CAVE_SPIDER) {
+
+    init {
+        addInFightAction(0 until 10) { spider ->
+            val target = spider.target ?: return@addInFightAction
+            val spiderLoc = spider.location.toVector()
+            val targetLoc = target.location.add(0.0, 0.5, 0.0).toVector()
+            val webLocation = targetLoc.clone().subtract(spiderLoc).normalize().multiply(0.3)
+            val web = spider.world.spawnFallingBlock(spider.location.add(webLocation), Material.COBWEB.createBlockData())
+            web.velocity = targetLoc.clone().subtract(spiderLoc).multiply(0.2)
+        }
+        addInFightAction(0 until 20) { spider ->
+            val target = spider.target ?: return@addInFightAction
+            spider.jump(target.location)
+        }
     }
-    
+
     @EventHandler
     fun onLand(event: EntityChangeBlockEvent) {
         val entity = event.entity as? FallingBlock ?: return
@@ -59,7 +41,7 @@ class EnchantedSpider: Listener {
             }.runTaskLater(instance, 20 * 5)
         }
     }
-    
+
     @EventHandler
     fun onDrop(event: EntityDropItemEvent) {
         val entity = event.entity as? FallingBlock ?: return
@@ -69,7 +51,7 @@ class EnchantedSpider: Listener {
             entity.remove()
         }
     }
-    
+
     @EventHandler
     fun onDamage(event: EntityDamageEvent) {
         val spider = event.entity as? Spider ?: return
