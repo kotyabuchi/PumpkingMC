99b918630d99fb585cb39d3e3693efe5f4ddb405
kunugiya <kunugiya6280@gmail.com>
2021-01-28 15:47:59 +0900

    Change: Endermanがブロックを引っこ抜かないように変更

--- src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Entity/Monster/EnchantedEnderman.kt
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Entity/Monster/EnchantedEnderman.kt
@@ -7,6 +7,7 @@ import org.bukkit.Sound
 import org.bukkit.entity.Enderman
 import org.bukkit.entity.EntityType
 import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.EntityChangeBlockEvent
 import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.event.entity.EntityTargetEvent
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent
@@ -49,10 +50,16 @@ class EnchantedEnderman: MobExpansionMaster(EntityType.ENDERMAN) {
     }
 
     @EventHandler
-    fun damagedCuredEye(event: EntityDamageEvent) {
+    fun onDamagedCursedEye(event: EntityDamageEvent) {
         val entity = event.entity
         if (entity.customName == "Cursed Eye") {
             entity.world.playSound(entity.location, Sound.BLOCK_END_PORTAL_FRAME_FILL, .5f, .5f)
         }
+    }
+
+    @EventHandler
+    fun onPopBlock(event: EntityChangeBlockEvent) {
+        val entity = event.entity
+        if (entity is Enderman) event.isCancelled = true
     }
 }
