bc1188a5aebcccf9828a20c9c1f62411dcc39abe
kunugiya <kunugiya6280@gmail.com>
2021-01-28 18:55:23 +0900

    Add: PDCのタグをトグル出来る拡張関数を追加

--- src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Utility/PlayerUtil.kt
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Utility/PlayerUtil.kt
@@ -1,8 +1,22 @@
 package com.github.kotyabuchi.pumpkingmc.Utility
 
+import com.github.kotyabuchi.pumpkingmc.instance
+import org.bukkit.NamespacedKey
 import org.bukkit.entity.Player
+import org.bukkit.persistence.PersistentDataType
 
 fun Player.sendActionMessage(message: String) {
     val blank = "                                   "
     this.sendMessage(blank.substring(0 until (blank.length - (message.length / 2))) + message.colorS())
+}
+
+fun Player.toggleTag(tagName: String): Boolean {
+    val pdc = this.persistentDataContainer
+    return if (pdc.has(NamespacedKey(instance, tagName), PersistentDataType.BYTE)) {
+        pdc.remove(NamespacedKey(instance, tagName))
+        false
+    } else {
+        pdc.set(NamespacedKey(instance, tagName), PersistentDataType.BYTE, 1)
+        true
+    }
 }
