1f3744f7c2ae7791379778e22bd5afb83201cb62
kunugiya <kunugiya6280@gmail.com>
2021-01-29 16:28:21 +0900

    Add: ExcavationにMultiBreakを追加

--- src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Class/Lifestyle/Excavation.kt
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Class/Lifestyle/Excavation.kt
@@ -7,17 +7,29 @@ import com.github.kotyabuchi.pumpkingmc.Enum.JobClassType
 import com.github.kotyabuchi.pumpkingmc.Enum.SkillCommand
 import com.github.kotyabuchi.pumpkingmc.System.ItemExpansion
 import com.github.kotyabuchi.pumpkingmc.System.Player.getStatus
+import com.github.kotyabuchi.pumpkingmc.Utility.hasTag
 import com.github.kotyabuchi.pumpkingmc.Utility.sendActionMessage
+import com.github.kotyabuchi.pumpkingmc.Utility.toggleTag
 import com.github.kotyabuchi.pumpkingmc.instance
 import org.bukkit.Material
+import org.bukkit.NamespacedKey
+import org.bukkit.Particle
+import org.bukkit.Sound
+import org.bukkit.block.Block
+import org.bukkit.block.BlockFace
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.block.BlockDropItemEvent
+import org.bukkit.persistence.PersistentDataType
+import java.util.ArrayList
 import kotlin.math.floor
 import kotlin.math.min
 import kotlin.random.Random
 
 object Excavation: BlockBreakJobClass(JobClassType.EXCAVATION) {
+
+    private val multiBreakKey = name + "_MultiBreak"
 
     init {
         Material.values().forEach {
@@ -27,6 +39,15 @@ object Excavation: BlockBreakJobClass(JobClassType.EXCAVATION) {
 
         addAction(SkillCommand.RRR, 25, fun(player: Player) {
             SuperBreaker.enableSuperBreaker(player, jobClassType)
+        })
+        addAction(SkillCommand.LLR, 100, fun(player: Player) {
+            if (player.toggleTag(multiBreakKey)) {
+                player.playSound(player.location.add(0.0, 2.0, 0.0), Sound.ENTITY_PLAYER_LEVELUP, 0.2f, 2.0f)
+                player.sendActionBar('&', "&aMulti Break On")
+            } else {
+                player.playSound(player.location.add(0.0, 2.0, 0.0), Sound.ENTITY_PLAYER_LEVELUP, 0.2f, 2.0f)
+                player.sendActionBar('&', "&cMulti Break Off")
+            }
         })
     }
 
@@ -60,13 +81,73 @@ object Excavation: BlockBreakJobClass(JobClassType.EXCAVATION) {
     fun onBreak(event: BlockBreakEvent) {
         if (event is BlockMineEvent) return
         val player = event.player
+        val level = player.getStatus().getJobClassStatus(jobClassType).getLevel()
         val block = event.block
         val item = player.inventory.itemInMainHand
 
         if (!getTool().contains(item.type)) return
 
-        val mineEvent = BlockMineEvent(block, player)
-        instance.server.pluginManager.callEvent(mineEvent)
-        if (mineEvent.isCancelled) event.isCancelled = true
+        if (player.hasTag(multiBreakKey)) {
+            val blockList = ArrayList<Block>()
+            val rayBlock = player.rayTraceBlocks(6.0)?: return
+            val lookingFace = rayBlock.hitBlockFace ?: return
+            val radius = floor(level / 100.0).toInt()
+            val range = (radius * -1)..radius
+            when (lookingFace) {
+                BlockFace.UP, BlockFace.DOWN -> {
+                    for (x in range) {
+                        for (z in range) {
+                            val checkBlock = block.location.add(x.toDouble(), 0.0, z.toDouble()).block
+                            if (checkBlock.getDrops(item, player).isEmpty()) continue
+                            if (checkBlock.type.isAir) continue
+                            blockList.add(checkBlock)
+                        }
+                    }
+                }
+                BlockFace.SOUTH, BlockFace.NORTH -> {
+                    for (x in range) {
+                        for (y in -1 until (radius * 2)) {
+                            val checkBlock = block.location.add(x.toDouble(), y.toDouble(), 0.0).block
+                            if (checkBlock.getDrops(item, player).isEmpty()) continue
+                            if (checkBlock.type.isAir) continue
+                            blockList.add(checkBlock)
+                        }
+                    }
+                }
+                BlockFace.EAST, BlockFace.WEST -> {
+                    for (z in range) {
+                        for (y in -1 until (radius * 2)) {
+                            val checkBlock = block.location.add(0.0, y.toDouble(), z.toDouble()).block
+                            if (checkBlock.getDrops(item, player).isEmpty()) continue
+                            if (checkBlock.type.isAir) continue
+                            blockList.add(checkBlock)
+                        }
+                    }
+                }
+                else -> {
+                    player.sendMessage("Error")
+                }
+            }
+            blockList.forEach {
+                val mineEvent = BlockMineEvent(it, player, true)
+                instance.server.pluginManager.callEvent(mineEvent)
+                event.isCancelled = true
+                if (!mineEvent.isCancelled) {
+                    it.getDrops(item, player).forEach { item ->
+                        val dropItem = block.world.dropItemNaturally(block.location.add(0.5, 0.0, 0.5), item)
+                        val dropEvent = BlockDropItemEvent(it, it.state, player, mutableListOf(dropItem))
+                        instance.server.pluginManager.callEvent(dropEvent)
+                        if (dropEvent.items.isEmpty()) dropItem.remove()
+                    }
+                    it.world.playSound(it.location, it.soundGroup.breakSound, 1f, .75f)
+                    it.world.spawnParticle(Particle.BLOCK_CRACK, it.location.add(0.5, 0.5, 0.5), 20, .3, .3, .3, 2.0, it.blockData)
+                    it.type = Material.AIR
+                }
+            }
+        } else {
+            val mineEvent = BlockMineEvent(block, player)
+            instance.server.pluginManager.callEvent(mineEvent)
+            if (mineEvent.isCancelled) event.isCancelled = true
+        }
     }
 }
