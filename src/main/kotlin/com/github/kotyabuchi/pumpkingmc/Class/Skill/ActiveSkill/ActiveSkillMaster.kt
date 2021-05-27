cbd745fe0169dcf821e2b07628ee7172c3d9172b
kotyabuchi <kotyabuchi@gmail.com>
2021-05-27 16:19:54 +0900

    Change: ToggleSkillとActiveSkillを分割

--- src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Class/Skill/ActiveSkill/ActiveSkillMaster.kt
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Class/Skill/ActiveSkill/ActiveSkillMaster.kt
@@ -1,50 +1,27 @@
 package com.github.kotyabuchi.pumpkingmc.Class.Skill.ActiveSkill
 
-import com.github.kotyabuchi.pumpkingmc.Utility.hasTag
 import com.github.kotyabuchi.pumpkingmc.instance
-import org.bukkit.NamespacedKey
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Sound
 import org.bukkit.entity.Player
-import org.bukkit.event.Listener
 import org.bukkit.persistence.PersistentDataType
 import org.bukkit.scheduler.BukkitRunnable
 import org.bukkit.scheduler.BukkitTask
 import java.util.*
 
-interface ActiveSkillMaster: Listener {
-    val skillName: String
-    val cost: Int
+interface ActiveSkillMaster: ToggleSkillMaster {
     val coolTime: Long
-    val needLevel: Int
-    val description: String
     val hasActiveTime: Boolean
-    val activePlayerLevelMap: MutableMap<UUID, Int>
     val activeTimeMap: MutableMap<UUID, BukkitTask>
     val coolTimePlayers: MutableList<UUID>
 
-    fun getSkillNamespacedKey(): NamespacedKey = NamespacedKey(instance, skillName)
-
-    fun calcActiveTime(level: Int): Int
-
-    fun isEnabledSkill(player: Player): Boolean = player.hasTag(getSkillNamespacedKey())
-
-    fun enableAction(player: Player, level: Int)
-
-    fun disableAction(player: Player)
-
-    fun toggleSkill(player: Player, level: Int) {
-        if (isEnabledSkill(player)) {
-            disableSkill(player)
-        } else {
-            enableSkill(player, level)
-        }
-    }
-
-    fun enableSkill(player: Player, level: Int) {
+    override fun enableSkill(player: Player, level: Int) {
         val uuid = player.uniqueId
         if (needLevel > level) {
             player.playSound(player.location, Sound.ENTITY_BLAZE_SHOOT, 0.5f, 2f)
-            player.sendActionBar('&', "&cNot enough levels (Need Lv.$needLevel)")
+            player.sendActionBar(
+                Component.text("$skillName: Not enough levels (Need Lv.$needLevel)").color(NamedTextColor.RED))
         } else if (!isEnabledSkill(player)) {
             enableAction(player, level)
             activePlayerLevelMap[uuid] = level
@@ -59,7 +36,7 @@ interface ActiveSkillMaster: Listener {
         }
     }
 
-    fun disableSkill(player: Player) {
+    override fun disableSkill(player: Player) {
         val uuid = player.uniqueId
         disableAction(player)
         activeTimeMap[uuid]?.cancel()
