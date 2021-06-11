28c694cb217a3c3d5177a6dfe4a1b560abccf434
kotyabuchi <kotyabuchi@gmail.com>
2021-06-11 20:54:03 +0900

    Add: Projectileのエンチャント情報を管理するメソッドを追加

--- src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Utility/EntityUtil.kt
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Utility/EntityUtil.kt
@@ -1,7 +1,11 @@
 package com.github.kotyabuchi.pumpkingmc.Utility
 
+import com.github.kotyabuchi.pumpkingmc.CustomEnchantment.CustomEnchantment
 import org.bukkit.Location
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Entity
+import org.bukkit.entity.Projectile
+import org.bukkit.persistence.PersistentDataType
 
 fun Entity.jump(loc: Location) {
     val entityLoc = this.location
@@ -23,4 +27,30 @@ fun Entity.jump(loc: Location) {
         vel.z = -4.0
     }
     this.velocity = vel
+}
+
+fun Projectile.addEnchantment(enchant: Enchantment, level: Int) {
+    val pdc = this.persistentDataContainer
+    var enchantments = pdc.get(CustomEnchantment.EnchantmentKey, PersistentDataType.STRING)
+    if (enchantments == null) enchantments = "" else enchantments += ","
+    pdc.set(CustomEnchantment.EnchantmentKey, PersistentDataType.STRING, "$enchantments${enchant.key.value()}:$level")
+}
+
+fun Projectile.hasEnchantment(enchant: Enchantment): Boolean {
+    val pdc = this.persistentDataContainer
+    val enchantments = pdc.get(CustomEnchantment.EnchantmentKey, PersistentDataType.STRING)?.split(",") ?: return false
+    enchantments.forEach {
+        if (it.split(":")[0] == enchant.key.value()) return true
+    }
+    return false
+}
+
+fun Projectile.getEnchantLevel(enchant: Enchantment): Int? {
+    val pdc = this.persistentDataContainer
+    val enchantments = pdc.get(CustomEnchantment.EnchantmentKey, PersistentDataType.STRING)?.split(",") ?: return null
+    enchantments.forEach {
+        val enchantStatus = it.split(":")
+        if (enchantStatus[0] == enchant.key.value()) return Integer.parseInt(enchantStatus[1])
+    }
+    return null
 }
