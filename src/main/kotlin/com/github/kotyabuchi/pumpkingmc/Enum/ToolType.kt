985cf209bf2239c36722120b7eb5f8a17d18f3a9
kotyabuchi <kotyabuchi@gmail.com>
2021-06-14 15:47:29 +0900

    Add: ToolTypeのベースとなるEquipmentTypeを追加

--- src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Enum/ToolType.kt
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Enum/ToolType.kt
@@ -1,19 +1,20 @@
 package com.github.kotyabuchi.pumpkingmc.Enum
 
 import org.bukkit.Material
-import org.bukkit.inventory.ItemStack
 
-enum class ToolType {
-    PICKAXE {
+enum class ToolType(override val materialCost: Int?): EquipmentType {
+    PICKAXE(3) {
         override fun includes(item: Material): Boolean {
-            return item == Material.WOODEN_PICKAXE || 
+            return item == Material.WOODEN_PICKAXE ||
                     item == Material.STONE_PICKAXE ||
                     item == Material.IRON_PICKAXE ||
                     item == Material.GOLDEN_PICKAXE ||
                     item == Material.DIAMOND_PICKAXE ||
                     item == Material.NETHERITE_PICKAXE
-        }},
-    AXE {
+        }
+    },
+    AXE(3) {
+
         override fun includes(item: Material): Boolean {
             return item == Material.WOODEN_AXE ||
                     item == Material.STONE_AXE ||
@@ -21,8 +22,9 @@ enum class ToolType {
                     item == Material.GOLDEN_AXE ||
                     item == Material.DIAMOND_AXE ||
                     item == Material.NETHERITE_AXE
-        }},
-    SHOVEL {
+        }
+    },
+    SHOVEL(1) {
         override fun includes(item: Material): Boolean {
             return item == Material.WOODEN_SHOVEL ||
                     item == Material.STONE_SHOVEL ||
@@ -30,8 +32,9 @@ enum class ToolType {
                     item == Material.GOLDEN_SHOVEL ||
                     item == Material.DIAMOND_SHOVEL ||
                     item == Material.NETHERITE_SHOVEL
-        }},
-    HOE {
+        }
+    },
+    HOE(2) {
         override fun includes(item: Material): Boolean {
             return item == Material.WOODEN_HOE ||
                     item == Material.STONE_HOE ||
@@ -39,8 +42,9 @@ enum class ToolType {
                     item == Material.GOLDEN_HOE ||
                     item == Material.DIAMOND_HOE ||
                     item == Material.NETHERITE_HOE
-        }},
-    SWORD {
+        }
+    },
+    SWORD(2) {
         override fun includes(item: Material): Boolean {
             return item == Material.WOODEN_SWORD ||
                     item == Material.STONE_SWORD ||
@@ -48,8 +52,9 @@ enum class ToolType {
                     item == Material.GOLDEN_SWORD ||
                     item == Material.DIAMOND_SWORD ||
                     item == Material.NETHERITE_SWORD
-        }},
-    BATTLEAXE {
+        }
+    },
+    BATTLEAXE(3) {
         override fun includes(item: Material): Boolean {
             return item == Material.WOODEN_AXE ||
                     item == Material.STONE_AXE ||
@@ -57,18 +62,29 @@ enum class ToolType {
                     item == Material.GOLDEN_AXE ||
                     item == Material.DIAMOND_AXE ||
                     item == Material.NETHERITE_AXE
-        }},
-    BOW {
+        }
+    },
+    BOW(3) {
         override fun includes(item: Material): Boolean {
             return item == Material.BOW ||
                     item == Material.CROSSBOW
-        }},
-    TRIDENT {
+        }
+    },
+    TRIDENT(null) {
         override fun includes(item: Material): Boolean {
             return item == Material.TRIDENT
-        }};
-    
-    abstract fun includes(item: Material): Boolean
-    
-    fun includes(item: ItemStack): Boolean = includes(item.type)
+        }
+    },
+    SHIELD(2) {
+        override fun includes(item: Material): Boolean {
+            return item == Material.SHIELD
+        }
+    },
+    FISHING_ROD(2) {
+        override fun includes(item: Material): Boolean {
+            return item == Material.FISHING_ROD
+        }
+    };
+
+    override fun getEquipmentName(): String = name
 }
