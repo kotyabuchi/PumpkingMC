3e917270b0794cae861589c8fa3e36c65f98e5ed
kotyabuchi <kotyabuchi@gmail.com>
2021-06-16 12:26:58 +0900

    Change: CursedEyeの攻撃をアーマー貫通の1に変更

--- src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Entity/Monster/CustomEntity/CursedEye.kt
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Entity/Monster/CustomEntity/CursedEye.kt
@@ -1,13 +1,22 @@
 package com.github.kotyabuchi.pumpkingmc.Entity.Monster.CustomEntity
 
 import com.github.kotyabuchi.pumpkingmc.Utility.createHead
+import com.github.kotyabuchi.pumpkingmc.instance
 import org.bukkit.attribute.Attribute
 import org.bukkit.entity.Enderman
+import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Vex
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
 
-object CursedEye {
+object CursedEye: Listener {
+
+    init {
+        instance.registerEvent(this)
+    }
 
     fun spawn(owner: Enderman) {
         val eye = owner.world.spawn(owner.location.add(.0, 1.0, .0), Vex::class.java) { eye ->
@@ -16,6 +25,7 @@ object CursedEye {
             eye.addPotionEffect(PotionEffect(PotionEffectType.INVISIBILITY, Int.MAX_VALUE, 1, false, false))
             eye.addPotionEffect(PotionEffect(PotionEffectType.SLOW, Int.MAX_VALUE, 1, false, false))
             eye.getAttribute(Attribute.GENERIC_MAX_HEALTH)?.baseValue = 1.0
+            eye.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE)?.baseValue = 1.0
             eye.health = 1.0
 
             eye.equipment?.helmetDropChance = 0f
@@ -24,5 +34,16 @@ object CursedEye {
             eye.equipment?.setItemInMainHand(null)
         }
         eye.target = owner.target
+    }
+
+    @EventHandler
+    fun onDamage(event: EntityDamageByEntityEvent) {
+        val entity = event.entity as? LivingEntity ?: return
+        val damager = event.damager as? Vex ?:return
+        if (damager.customName == "Cursed Eye") {
+            event.isCancelled = true
+            entity.damage(1.0)
+            entity.noDamageTicks = 0
+        }
     }
 }
