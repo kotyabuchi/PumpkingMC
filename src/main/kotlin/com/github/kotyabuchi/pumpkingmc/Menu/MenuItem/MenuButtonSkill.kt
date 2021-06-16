cb2e5dce4b90b5c7f015f7fbeb914dfb155b3914
kotyabuchi <kotyabuchi@gmail.com>
2021-06-16 15:25:05 +0900

    Add: SkillMenuの必要経験値に現在の達成度を％で表記

--- src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Menu/MenuItem/MenuButtonSkill.kt
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Menu/MenuItem/MenuButtonSkill.kt
@@ -4,11 +4,13 @@ import com.github.kotyabuchi.pumpkingmc.Enum.JobClassType
 import com.github.kotyabuchi.pumpkingmc.Menu.SkillInfoMenu
 import com.github.kotyabuchi.pumpkingmc.System.Player.getStatus
 import com.github.kotyabuchi.pumpkingmc.Utility.ItemStackGenerator
+import com.github.kotyabuchi.pumpkingmc.Utility.floor1Digits
 import com.github.kotyabuchi.pumpkingmc.Utility.upperCamelCase
 import com.github.kotyabuchi.pumpkingmc.Utility.floor2Digits
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.inventory.ItemFlag
+import kotlin.math.round
 
 class MenuButtonSkill(private val jobClassType: JobClassType, player: Player): MenuButtonBase() {
 
@@ -18,9 +20,10 @@ class MenuButtonSkill(private val jobClassType: JobClassType, player: Player): M
         val jobClassStatus = player.getStatus().getJobClassStatus(jobClassType.jobClass)
         val lore = mutableListOf<String>()
         lore.add("Level: ${jobClassStatus.getLevel()}")
-        lore.add("Need Exp: ${jobClassStatus.getExp().floor2Digits()}/${jobClassStatus.getNextLevelExp()}")
+        val exp = jobClassStatus.getExp().floor2Digits()
+        val nextLevel = jobClassStatus.getNextLevelExp()
+        lore.add("Need Exp: $exp/$nextLevel [${(round(exp / nextLevel * 1000) / 10).floor1Digits()}%]")
         lore.add("Total Exp: ${jobClassStatus.getTotalExp().floor2Digits()}")
-        lore.add("")
         menuItem = ItemStackGenerator(jobClassType.getIcon()).setDisplayName(jobClassType.name.upperCamelCase()).setFlag(ItemFlag.HIDE_ATTRIBUTES).setLore(lore).setMenuItemTag().generate()
     }
 
