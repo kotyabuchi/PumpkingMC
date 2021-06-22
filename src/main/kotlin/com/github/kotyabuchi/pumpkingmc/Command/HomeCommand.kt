0d37fb0d48609cafa2c6d416beabe1bc0966eaa7
kotyabuchi <kotyabuchi@gmail.com>
2021-06-22 10:56:15 +0900

    Change: 表示メッセージをComponentに対応

--- src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Command/HomeCommand.kt
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/Command/HomeCommand.kt
@@ -1,25 +1,20 @@
 package com.github.kotyabuchi.pumpkingmc.Command
 
-import com.eclipsesource.json.Json
-import com.eclipsesource.json.JsonObject
 import com.github.kotyabuchi.pumpkingmc.Menu.HomeMenu
 import com.github.kotyabuchi.pumpkingmc.System.Player.getStatus
+import com.github.kotyabuchi.pumpkingmc.Utility.ChatUtil
 import com.github.kotyabuchi.pumpkingmc.Utility.addHome
 import com.github.kotyabuchi.pumpkingmc.Utility.colorS
-import com.github.kotyabuchi.pumpkingmc.Utility.readFile
+import com.github.kotyabuchi.pumpkingmc.Utility.normal
 import com.github.kotyabuchi.pumpkingmc.instance
-import io.reactivex.rxjava3.core.Observable
-import org.bukkit.Location
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.command.TabCompleter
 import org.bukkit.entity.Player
-import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.player.PlayerJoinEvent
-import java.io.File
-import java.util.concurrent.TimeUnit
 
 class HomeCommand: CommandExecutor, TabCompleter, Listener {
 
@@ -47,41 +42,20 @@ class HomeCommand: CommandExecutor, TabCompleter, Listener {
             val homeId = addHome(sender, args[1], sender.location)
 
             if (homeId == null) {
-                sender.sendMessage("&eHomePoint [${args[1]}] の登録に失敗しました".colorS())
+                sender.sendMessage(Component.text("HomePoint [${args[1]}] の登録に失敗しました", NamedTextColor.RED).normal())
             } else {
-                sender.sendMessage("&2HomePoint [${args[1]}] を登録しました".colorS())
+                sender.sendMessage(Component.text("HomePoint [${args[1]}] を登録しました", NamedTextColor.GREEN).normal())
             }
             return true
         }
-        sender.sendMessage("&ahome: &r登録されているHomePointを表示します\n&ahome set <Name>: &r新しくHomePointを登録します\n&ahome remove: &rHomePointの削除メニューを表示する".colorS())
+        sender.sendMessage(ChatUtil.separator("Home"))
+        sender.sendMessage(Component.text("/home: ", NamedTextColor.WHITE).normal()
+            .append(Component.text("登録されているHomePoint一覧を表示します", NamedTextColor.GRAY)))
+        sender.sendMessage(Component.text("/home set <Name>: ", NamedTextColor.WHITE).normal()
+            .append(Component.text("<Name>の名前で新しくHomePointを登録します", NamedTextColor.GRAY)))
+        sender.sendMessage(Component.text("/home remove: ", NamedTextColor.WHITE).normal()
+            .append(Component.text("HomePointの削除用メニューを表示します", NamedTextColor.GRAY)))
+        sender.sendMessage(ChatUtil.separator("Home", footer = true))
         return true
-    }
-
-    @EventHandler
-    fun onJoin(event: PlayerJoinEvent) {
-        val player = event.player
-        val homeFolder = File(instance.dataFolder, "homes")
-        if (!homeFolder.exists()) return
-        if (homeFolder.listFiles() == null || homeFolder.listFiles().isEmpty()) {
-            homeFolder.delete()
-            return
-        }
-        val file = File(instance.dataFolder, "homes" + File.separator + player.uniqueId.toString() + ".json")
-        if (!file.exists()) return
-
-        val jsonArray = Json.parse(readFile(file)).asArray()
-        if (jsonArray.isEmpty) {
-            file.delete()
-        } else {
-            jsonArray.forEach {
-                it as JsonObject
-                instance.server.getWorld(it.getString("World", "world"))?.let {  world ->
-                    addHome(player, it.getString("Name", ""), Location(world, it.getDouble("X", .0), it.getDouble("Y", .0), it.getDouble("Z", .0), it.getFloat("Yaw", 0f), 0f))
-                }
-            }
-            Observable.interval(10, TimeUnit.MILLISECONDS).take(1).subscribe {
-                file.delete()
-            }
-        }
     }
 }
