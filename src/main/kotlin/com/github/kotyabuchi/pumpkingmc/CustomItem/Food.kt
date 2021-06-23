4d760aaf3d003b8b47dd086305a9f264b827b176
kotyabuchi <kotyabuchi@gmail.com>
2021-06-23 13:33:22 +0900

    Add: 食べ物系アイテムのクラスを追加

---
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/CustomItem/Food.kt
@@ -0,0 +1,25 @@
+package com.github.kotyabuchi.pumpkingmc.CustomItem
+
+import net.kyori.adventure.text.Component
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+
+abstract class Food: ConsumableItem() {
+
+    abstract val foodLevel: Int
+    abstract val exhaustion: Float
+
+    override fun createItemStack(): ItemStack {
+        val itemStack = super.createItemStack()
+        itemStack.editMeta {
+            val lore = it.lore() ?: mutableListOf()
+            lore.add(Component.text(""))
+        }
+        return itemStack
+    }
+
+    override fun consume(player: Player) {
+        player.foodLevel += foodLevel
+        player.exhaustion -= exhaustion
+    }
+}
