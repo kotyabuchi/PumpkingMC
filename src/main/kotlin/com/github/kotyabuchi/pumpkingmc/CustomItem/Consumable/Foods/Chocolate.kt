c9d2c9215cbbe1fe7a699ad8289f74606af2f72c
kotyabuchi <kotyabuchi@gmail.com>
2021-06-23 15:12:16 +0900

    Add: 新しい食べ物のChocolateを追加

---
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/CustomItem/Consumable/Foods/Chocolate.kt
@@ -0,0 +1,38 @@
+package com.github.kotyabuchi.pumpkingmc.CustomItem.Consumable.Foods
+
+import com.github.kotyabuchi.pumpkingmc.CustomItem.Food
+import com.github.kotyabuchi.pumpkingmc.System.ItemExpansion
+import com.github.kotyabuchi.pumpkingmc.Utility.addItemOrDrop
+import com.github.kotyabuchi.pumpkingmc.Utility.normal
+import com.github.kotyabuchi.pumpkingmc.instance
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.TextComponent
+import org.bukkit.Bukkit.addRecipe
+import org.bukkit.GameMode
+import org.bukkit.Material
+import org.bukkit.NamespacedKey
+import org.bukkit.entity.Player
+import org.bukkit.event.player.PlayerItemConsumeEvent
+import org.bukkit.inventory.ShapedRecipe
+
+object Chocolate: Food() {
+
+    override val itemName: TextComponent = Component.text("Chocolate").normal()
+    override val modelData: Int = 101
+    override val material: Material = Material.APPLE
+    override val foodLevel: Int = 2
+    override val exhaustion: Float = 0f
+
+    init {
+        val recipe = ShapedRecipe(NamespacedKey(instance, itemType), itemStack)
+            .shape("AAA", " B ")
+            .setIngredient('A', Material.COCOA_BEANS)
+            .setIngredient('B', Material.GLASS_BOTTLE)
+        addRecipe(recipe)
+    }
+
+    override fun consume(player: Player, event: PlayerItemConsumeEvent?) {
+        super.consume(player, event)
+        if (player.gameMode != GameMode.CREATIVE) player.inventory.addItemOrDrop(player, ItemExpansion(Material.GLASS_BOTTLE).item)
+    }
+}
--- src/main/kotlin/com/github/kotyabuchi/pumpkingmc/CustomItem/CustomItem.kt
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/CustomItem/CustomItem.kt
@@ -1,7 +1,9 @@
 package com.github.kotyabuchi.pumpkingmc.CustomItem
 
+import com.github.kotyabuchi.pumpkingmc.CustomItem.Consumable.Foods.Chocolate
 import com.github.kotyabuchi.pumpkingmc.CustomItem.Consumable.Foods.HoneyedApple
 
 enum class CustomItem(val itemClass: ItemMaster) {
+    CHOCOLATE(Chocolate),
     HONEYED_APPLE(HoneyedApple)
 }
