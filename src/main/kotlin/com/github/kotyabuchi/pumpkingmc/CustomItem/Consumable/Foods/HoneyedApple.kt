d46ecdc20e934759784701721bf4ac0c23810a56
kotyabuchi <kotyabuchi@gmail.com>
2021-06-23 13:38:22 +0900

    Add: 新しい食べ物のHoneyedAppleを追加

---
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/CustomItem/Consumable/Foods/HoneyedApple.kt
@@ -0,0 +1,37 @@
+package com.github.kotyabuchi.pumpkingmc.CustomItem.Consumable.Foods
+
+import com.github.kotyabuchi.pumpkingmc.CustomItem.Craftable
+import com.github.kotyabuchi.pumpkingmc.CustomItem.Food
+import com.github.kotyabuchi.pumpkingmc.Utility.normal
+import com.github.kotyabuchi.pumpkingmc.instance
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.TextComponent
+import org.bukkit.Material
+import org.bukkit.NamespacedKey
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ShapelessRecipe
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
+
+object HoneyedApple: Food(), Craftable {
+
+    override val itemName: TextComponent = Component.text("Honeyed Apple").normal()
+    override val modelData: Int = 100
+    override val material: Material = Material.APPLE
+    override val foodLevel: Int = 14
+    override val exhaustion: Float = 0f
+
+    init {
+        val recipe = ShapelessRecipe(NamespacedKey(instance, "Honeyed_Apple"), itemStack)
+        repeat(8) {
+            recipe.addIngredient(Material.APPLE)
+        }
+        recipe.addIngredient(Material.HONEY_BOTTLE)
+        addRecipe(recipe)
+    }
+
+    override fun consume(player: Player) {
+        super.consume(player)
+        player.addPotionEffect(PotionEffect(PotionEffectType.REGENERATION, 20 * 5, 1))
+    }
+}
