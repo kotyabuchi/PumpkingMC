97b81e601c29f10cb83e1ff0a029594715c05a57
kotyabuchi <kotyabuchi@gmail.com>
2021-06-29 23:14:32 +0900

    Change: ProcessingRecipeがNamespacedKeyを要求するように

--- src/main/kotlin/com/github/kotyabuchi/pumpkingmc/AdvancedProcessing/Recipe/CookingPotRecipe.kt
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/AdvancedProcessing/Recipe/CookingPotRecipe.kt
@@ -1,9 +1,10 @@
 package com.github.kotyabuchi.pumpkingmc.AdvancedProcessing.Recipe
 
 import com.github.kotyabuchi.pumpkingmc.AdvancedProcessing.ProcessingType
+import org.bukkit.NamespacedKey
 import org.bukkit.inventory.ItemStack
 
-class CookingPotRecipe(private val result: ItemStack, vararg val ingredients: ItemStack): ProcessingRecipe {
+class CookingPotRecipe(override val recipeKey: NamespacedKey, private val result: ItemStack, vararg val ingredients: ItemStack): ProcessingRecipe {
 
     override val processingType: ProcessingType = ProcessingType.COOKING_POT
     override fun getResult(): ItemStack {
--- src/main/kotlin/com/github/kotyabuchi/pumpkingmc/AdvancedProcessing/Recipe/ProcessingRecipe.kt
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/AdvancedProcessing/Recipe/ProcessingRecipe.kt
@@ -1,9 +1,11 @@
 package com.github.kotyabuchi.pumpkingmc.AdvancedProcessing.Recipe
 
 import com.github.kotyabuchi.pumpkingmc.AdvancedProcessing.ProcessingType
+import org.bukkit.NamespacedKey
 import org.bukkit.inventory.Recipe
 
 interface ProcessingRecipe: Recipe {
 
+    val recipeKey: NamespacedKey
     val processingType: ProcessingType
 }
