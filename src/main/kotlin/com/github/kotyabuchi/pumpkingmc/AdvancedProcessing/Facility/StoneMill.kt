f40c6dabd0636b9c96b2fe18bc8f2dc5dd7e70f6
kotyabuchi <kotyabuchi@gmail.com>
2021-06-29 16:08:47 +0900

    Add: StoneMillの仮追加

--- src/main/kotlin/com/github/kotyabuchi/pumpkingmc/AdvancedProcessing/Facility/StoneMill.kt
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/AdvancedProcessing/Facility/StoneMill.kt
@@ -1,4 +1,15 @@
 package com.github.kotyabuchi.pumpkingmc.AdvancedProcessing.Facility
 
-object StoneMill {
+import com.github.kotyabuchi.pumpkingmc.AdvancedProcessing.Menu.AlchemyCauldronMenu
+import com.github.kotyabuchi.pumpkingmc.Menu.MenuBase
+import com.github.kotyabuchi.pumpkingmc.Utility.ItemStackGenerator
+import org.bukkit.Material
+import org.bukkit.inventory.EquipmentSlot
+import org.bukkit.inventory.ItemStack
+
+object StoneMill: ProcessingFacility {
+
+    private val modelItem = ItemStackGenerator(Material.GOLDEN_HOE).setModelData(102).generate()
+    override val facilityModelItemMap: Map<EquipmentSlot, ItemStack> = mapOf(EquipmentSlot.HEAD to modelItem)
+    override val facilityMenu: MenuBase = AlchemyCauldronMenu()
 }
--- src/main/kotlin/com/github/kotyabuchi/pumpkingmc/AdvancedProcessing/ProcessingType.kt
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/AdvancedProcessing/ProcessingType.kt
@@ -3,8 +3,10 @@ package com.github.kotyabuchi.pumpkingmc.AdvancedProcessing
 import com.github.kotyabuchi.pumpkingmc.AdvancedProcessing.Facility.AlchemyCauldron
 import com.github.kotyabuchi.pumpkingmc.AdvancedProcessing.Facility.CookingPot
 import com.github.kotyabuchi.pumpkingmc.AdvancedProcessing.Facility.ProcessingFacility
+import com.github.kotyabuchi.pumpkingmc.AdvancedProcessing.Facility.StoneMill
 
 enum class ProcessingType(facility: ProcessingFacility) {
     ALCHEMY_CAULDRON(AlchemyCauldron),
-    COOKING_POT(CookingPot)
+    COOKING_POT(CookingPot),
+    STONE_MILL(StoneMill)
 }
