e6017877edd4e84e19f13ca16885e4568fb7043f
kotyabuchi <kotyabuchi@gmail.com>
2021-06-29 23:15:52 +0900

    Add: RecipeBookを表示するボタンの仮追加

---
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/AdvancedProcessing/Menu/Button/RecipeBookButton.kt
@@ -0,0 +1,10 @@
+package com.github.kotyabuchi.pumpkingmc.AdvancedProcessing.Menu.Button
+
+import com.github.kotyabuchi.pumpkingmc.AdvancedProcessing.ProcessingType
+import com.github.kotyabuchi.pumpkingmc.Menu.MenuItem.MenuButtonBase
+import org.bukkit.entity.Player
+
+class RecipeBookButton(private val processingType: ProcessingType, private val player: Player): MenuButtonBase() {
+
+
+}
