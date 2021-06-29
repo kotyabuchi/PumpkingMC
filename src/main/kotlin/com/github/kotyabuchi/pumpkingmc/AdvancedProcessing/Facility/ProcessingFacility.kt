4bc75844ebc53830c6d03799f6c1be30524cf3da
kotyabuchi <kotyabuchi@gmail.com>
2021-06-29 13:35:39 +0900

    Change: Model用アイテムをスロット別に分割することが出来るように変更

--- src/main/kotlin/com/github/kotyabuchi/pumpkingmc/AdvancedProcessing/Facility/ProcessingFacility.kt
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/AdvancedProcessing/Facility/ProcessingFacility.kt
@@ -3,11 +3,12 @@ package com.github.kotyabuchi.pumpkingmc.AdvancedProcessing.Facility
 import com.github.kotyabuchi.pumpkingmc.Menu.MenuBase
 import com.github.kotyabuchi.pumpkingmc.System.Player.getStatus
 import org.bukkit.entity.Player
+import org.bukkit.inventory.EquipmentSlot
 import org.bukkit.inventory.ItemStack
 
 interface ProcessingFacility {
 
-    val facilityModelItem: ItemStack
+    val facilityModelItemMap: Map<EquipmentSlot, ItemStack>
     val facilityMenu: MenuBase
 
     fun openFacilityMenu(player: Player) {
