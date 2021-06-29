01270907996fc832464f97fa9959c1630f8b4fee
kotyabuchi <kotyabuchi@gmail.com>
2021-06-29 13:37:39 +0900

    Add: AlchemyCauldronのがわを追加

--- src/main/kotlin/com/github/kotyabuchi/pumpkingmc/AdvancedProcessing/Facility/AlchemyCauldron.kt
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/AdvancedProcessing/Facility/AlchemyCauldron.kt
@@ -1,4 +1,13 @@
 package com.github.kotyabuchi.pumpkingmc.AdvancedProcessing.Facility
 
-object AlchemyCauldron {
+import com.github.kotyabuchi.pumpkingmc.AdvancedProcessing.Menu.AlchemyCauldronMenu
+import com.github.kotyabuchi.pumpkingmc.Menu.MenuBase
+import org.bukkit.Material
+import org.bukkit.inventory.EquipmentSlot
+import org.bukkit.inventory.ItemStack
+
+object AlchemyCauldron: ProcessingFacility {
+
+    override val facilityModelItemMap: Map<EquipmentSlot, ItemStack> = mapOf(EquipmentSlot.HEAD to ItemStack(Material.GOLDEN_HOE))
+    override val facilityMenu: MenuBase = AlchemyCauldronMenu()
 }
---
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/AdvancedProcessing/Menu/AlchemyCauldronMenu.kt
@@ -0,0 +1,8 @@
+package com.github.kotyabuchi.pumpkingmc.AdvancedProcessing.Menu
+
+import com.github.kotyabuchi.pumpkingmc.Menu.MenuBase
+
+class AlchemyCauldronMenu: MenuBase("Alchemy Cauldron", 6) {
+
+
+}
