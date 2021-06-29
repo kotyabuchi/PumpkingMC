09e316711679d423fe638f0b20f70ae0181bc2bc
kotyabuchi <kotyabuchi@gmail.com>
2021-06-29 14:52:58 +0900

    Add: CookingPotMenuのがわを追加

---
+++ src/main/kotlin/com/github/kotyabuchi/pumpkingmc/AdvancedProcessing/Menu/CookingPotMenu.kt
@@ -0,0 +1,8 @@
+package com.github.kotyabuchi.pumpkingmc.AdvancedProcessing.Menu
+
+import com.github.kotyabuchi.pumpkingmc.Menu.MenuBase
+
+class CookingPotMenu: MenuBase("Cooking Pot", 6) {
+
+
+}
